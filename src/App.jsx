a59732edc95c85cc37e9580b2dbd762bd228007b
codeinfinite19 <shahedhassan572@gmail.com>
2026-04-03 11:55:40 +0600

    basic stucture of project is created and nav bar is running to implement

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+
+//https://www.themealdb.com/api/json/v1/1/search.php?f=a").then(res=> res.json()
+
+
+
+
+function App() {
+
+ 
+  
+
+  return (
+    <>
+    <Navbar />
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <nav className="navbar">
+
+                <div>
+                    <img src="" alt="" />
+                    <h1>Meal Hub</h1>
+                </div>
+
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>Meals</li>
+                        <li>Order</li>
+                        <li>Blog</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+
+
+
+                </nav>
+            
+        </div>
+    );
+};
+
+export default Navbar;
